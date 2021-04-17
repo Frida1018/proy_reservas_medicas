0dcb16cdfb4c3628cff6d62b1d466c451fee32c7
Frida1018 <ximena.frida.sarzuri@gmail.com>
2021-04-17 07:34:42 -0400

    se iso la creacion del archivo de documentacion md del proyecto

---
+++ Documentacion/documentacio.md
@@ -0,0 +1,6 @@
+# proyecto reserva de citas medicas
+## Introduccion
+## Funcionalidades Principales
+## Casos de Uso
+![ caso de uso del sistema] (caso.jpg)
+
--- js/script.js
+++ js/script.js
@@ -1,54 +1,67 @@
-document.getElementById("btn_Registrarse").addEventListener("click",register);
-document.getElementById("btn_Iniciar_Secion").addEventListener("click",iniciarSecion);
+document.getElementById("btn_Registrarse").addEventListener("click", register);
+document.getElementById("btn_Iniciar_Secion").addEventListener("click", iniciarSecion);
+window.addEventListener("resizer", anchopagina);
 
 
 
 //declaracion de variables//
-var contenedor_login= document.querySelector(".contenedor_login")
-var form_login= document.querySelector(".form_login")
-var form_register= document.querySelector(".form_register")
-var caja_trasera_login= document.querySelector(".caja_trasera-login")
-var caja_trasera_register= document.querySelector("caja_trasera_register")
+var contenedor_login = document.querySelector(".contenedor_login")
+var form_login = document.querySelector(".form_login")
+var form_register = document.querySelector(".form_register")
+var caja_trasera_login = document.querySelector(".caja_trasera-login")
+var caja_trasera_register = document.querySelector("caja_trasera_register")
 
-function iniciarSecion(){
-
-    if(window.innerWidth>850){
-       form_register.style.display ="none";
-       contenedor_login.style.left ="10px";
-       form_login.style.display ="block";
-       caja_trasera_login.style.opacity ="1";
-       caja_trasera_register.style.opacity ="0";
+function anchopagina(){
+    if(window.innerWidth > 850) {
+        caja_trasera_login.style.display="block";
+        caja_trasera_register.style.display="block";
     }else{
-        form_register.style.display ="none";
-        contenedor_login.style.left ="0px";
-        form_login.style.display ="block";
-        caja_trasera_login.style.display ="block";
-        caja_trasera_register.style.display ="none";
-     }
+        caja_trasera_register.style.display="block";
+        caja_trasera_register.style.opacity="1";
+        caja_trasera_login.style.display="none";
+        form_login.style.display="block";
+        form_register.style.style="none";
+        contenedor_login.style.left="0px";
+    }
+}
 
- }
 
+function iniciarSecion() {
 
-function register(){
-    if(window.innerWidth>850){
-          form_register.style.display ="block";
-          contenedor_login.style.left ="410px";
-          form_login.style.display ="none";
-          caja_trasera_login.style.opacity ="0";
-          caja_trasera_register.style.opacity ="1";
-        }
-        else{
-            form_register.style.display ="block";
-            contenedor_login.style.left ="0px";
-            form_login.style.display ="none";
-            caja_trasera_register.style.display ="none";
-            caja_trasera_login.style.display ="block";
-            caja_trasera_login.style.opacity ="1";
-            
-         }
-    
+    if (window.innerWidth > 850) {
+        form_register.style.display = "none";
+        contenedor_login.style.left = "10px";
+        form_login.style.display = "block";
+        caja_trasera_login.style.opacity = "1";
+        caja_trasera_register.style.opacity = "0";
+    } else {
+        form_register.style.display = "none";
+        contenedor_login.style.left = "0px";
+        form_login.style.display = "block";
+        caja_trasera_login.style.display = "block";
+        caja_trasera_register.style.display = "none";
     }
 
+}
 
 
+function register() {
+    if (window.innerWidth > 850) {
+        form_register.style.display = "block";
+        contenedor_login.style.left = "410px";
+        form_login.style.display = "none";
+        caja_trasera_login.style.opacity = "0";
+        caja_trasera_register.style.opacity = "1";
+    }
+    else {
+        form_register.style.display = "block";
+        contenedor_login.style.left = "0px";
+        form_login.style.display = "none";
+        caja_trasera_register.style.display = "none";
+        caja_trasera_login.style.display = "block";
+        caja_trasera_login.style.opacity = "1";
+
+    }
+
+}
 
